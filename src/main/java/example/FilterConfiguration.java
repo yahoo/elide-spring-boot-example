fa3a78e86271613b8c33c6d955ada9f57d668485
Aaron Klish <klish@verizonmedia.com>
2019-12-14 15:53:33 -0600

    Added logback logging configuration for common scenarios

---
+++ src/main/java/example/FilterConfiguration.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2019, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+
+package example;
+
+import ch.qos.logback.access.servlet.TeeFilter;
+import com.yahoo.elide.spring.config.ElideConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfiguration {
+
+    @Bean
+    @Autowired
+    public FilterRegistrationBean requestResponseFilter(ElideConfigProperties elideConfig) {
+        final FilterRegistrationBean<TeeFilter> filterRegBean = new FilterRegistrationBean<>();
+        TeeFilter filter = new TeeFilter();
+        filterRegBean.setFilter(filter);
+        filterRegBean.addUrlPatterns("/*");
+        filterRegBean.setName("Elide Request Response Filter");
+        filterRegBean.setAsyncSupported(Boolean.TRUE);
+        return filterRegBean;
+    }
+}
