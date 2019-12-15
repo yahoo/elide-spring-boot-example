e3a50f16c8d4ac4deb02665842e64558492424a9
Aaron Klish <klish@verizonmedia.com>
2019-12-14 20:16:27 -0600

    Minor cleanup

--- src/main/java/example/FilterConfiguration.java
+++ src/main/java/example/FilterConfiguration.java
@@ -7,8 +7,6 @@
 package example;
 
 import ch.qos.logback.access.servlet.TeeFilter;
-import com.yahoo.elide.spring.config.ElideConfigProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
