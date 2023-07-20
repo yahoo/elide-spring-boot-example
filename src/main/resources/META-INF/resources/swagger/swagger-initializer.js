8dee14984e4f5615256212cecaaab881ec9008e0
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2023-07-20 10:15:22 +0800

    Upgrade swagger-ui to version 5.1.3

---
+++ src/main/resources/META-INF/resources/swagger/swagger-initializer.js
@@ -0,0 +1,25 @@
+/*
+ * Source code adapted from:
+ * https://github.com/swagger-api/swagger-ui/blob/v5.1.3/dist/swagger-initializer.js
+ */
+
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "../doc",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
